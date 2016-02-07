8bf06f68199396d314e7b845cb42b58035b9419c
Eaden <_@eaden.net>
2016-02-07 12:49:14 +0000

    test image 4

--- app/index.js
+++ app/index.js
@@ -6,11 +6,11 @@ const TwoByTwoCards = React.createClass({
   render() {
     return (
       <View style={[styles.container, {flexDirection: 'row' }]}>
-        <View style={{ flex: 1 }}>
+        <View style={{flex :  1 }}>
           <View style={styles.matrixInner}><Text>One</Text></View>
           <View style={styles.matrixInner}><Text>Two</Text></View>
         </View>
-        <View style={{flex: 1}}>
+        <View style={{flex: 1}, {}}>
           <View style={styles.matrixInner}><Text>Three</Text></View>
           <View style={styles.matrixInner}><Text>Four</Text></View>
         </View>
